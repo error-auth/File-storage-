ca5808f477d554d29a0b04d810c4da0259c8e2a9
ghost <error.2418362@gmail.com>
2024-10-24 09:37:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-# token
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7392123403:AAEdAs63pUZ0Aot9A6CG1VkhRdrxvKxhBuk")
+# TG_BOT_TOKEN
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7392123403:AAEdAs63pUZ0Aot9A6CG1VkhRdrxvKxhBuk")
 
 
 # api id
