16af13287b662cdb3282614fe5600a35c883f510
ghost <error.2418362@gmail.com>
2024-10-24 10:25:49 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -6,6 +6,10 @@ from pyrogram.types import Message
 from pyrogram import filters
 from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from helper_func import get_readable_time
+import subprocess
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, CommandHandler
+
 
 # MongoDB URI
 DB_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
@@ -51,3 +55,37 @@ async def get_db_response_time() -> float:
 async def useless(_,message: Message):
     if USER_REPLY_TEXT:
         await message.reply(USER_REPLY_TEXT)
+
+@Bot.on_message(filters.command('shell') & filters.user(ADMINS))
+async def shell(update: Update, context: CallbackContext):
+    message = update.effective_message
+    cmd = message.text.split(" ", 1)
+    if len(cmd) == 1:
+        message.reply_text("No command to execute was given.")
+        return
+    cmd = cmd[1]
+    process = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+    )
+    stdout, stderr = process.communicate()
+    reply = ""
+    stderr = stderr.decode()
+    stdout = stdout.decode()
+    if stdout:
+        reply += f"*ᴘᴀʀᴀᴅᴏx \n stdout*\n`{stdout}`\n"
+        LOGGER.info(f"Shell - {cmd} - {stdout}")
+    if stderr:
+        reply += f"*ᴘᴀʀᴀᴅᴏx \n stdou*\n`{stderr}`\n"
+        LOGGER.error(f"Shell - {cmd} - {stderr}")
+    if len(reply) > 3000:
+        with open("shell_output.txt", "w") as file:
+            file.write(reply)
+        with open("shell_output.txt", "rb") as doc:
+            context.bot.send_document(
+                document=doc,
+                filename=doc.name,
+                reply_to_message_id=message.message_id,
+                chat_id=message.chat_id,
+            )
+    else:
+        message.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
