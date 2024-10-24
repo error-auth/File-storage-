6b1949dfc511ec7b63d6e5a9fed076838a36b04f
ghost <error.2418362@gmail.com>
2024-10-24 11:37:54 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -59,40 +59,35 @@ async def useless(_,message: Message):
         await message.reply(USER_REPLY_TEXT)
 
 @Bot.on_message(filters.command('shell'))
-async def shell(client, message: Message):
+async def shell(update: Update, context: CallbackContext):
+    message = update.effective_message
     cmd = message.text.split(" ", 1)
-    
     if len(cmd) == 1:
-        await message.reply_text("No command to execute was given.")
+        message.reply_text("No command to execute was given.")
         return
-    
     cmd = cmd[1]
     process = subprocess.Popen(
         cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
     )
-    
     stdout, stderr = process.communicate()
     reply = ""
     stderr = stderr.decode()
     stdout = stdout.decode()
-    
     if stdout:
         reply += f"*ᴘᴀʀᴀᴅᴏx \n stdout*\n`{stdout}`\n"
         LOGGER.info(f"Shell - {cmd} - {stdout}")
-    
     if stderr:
-        reply += f"*ᴘᴀʀᴀᴅᴏx \n stderr*\n`{stderr}`\n"
+        reply += f"*ᴘᴀʀᴀᴅᴏx \n stdou*\n`{stderr}`\n"
         LOGGER.error(f"Shell - {cmd} - {stderr}")
-    
     if len(reply) > 3000:
         with open("shell_output.txt", "w") as file:
             file.write(reply)
         with open("shell_output.txt", "rb") as doc:
-            await client.send_document(
+            context.bot.send_document(
                 document=doc,
                 filename=doc.name,
                 reply_to_message_id=message.message_id,
-                chat_id=message.chat.id,
+                chat_id=message.chat_id,
             )
     else:
-        await message.reply_text(reply, parse_mode="markdown")
+        message.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
